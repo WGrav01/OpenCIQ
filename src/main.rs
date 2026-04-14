9a085b8b7e6e458bdc7229c6f23806616f44b614
wgrav <wgrav@noreply.codeberg.org>
2026-04-14 10:00:45 -0400

    feat: main.rs file w/ clap, doesn't do anything yet
    Signed-off-by: wgrav <wgrav@noreply.codeberg.org>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,40 @@
+// Copyright (C) 2026 wgrav
+// 
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+// 
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+use clap::{Parser, Subcommand};
+use std::path::PathBuf;
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    #[command(subcommand)]
+    cmd: Commands
+}
+
+#[derive(Debug, Subcommand, Clone)]
+enum Commands {
+    Parse{
+        #[arg()]
+        file: PathBuf
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+
+    match args.cmd {
+        Commands::Parse{ .. } => todo!() // TODO: Implement token parser
+    }
 }
